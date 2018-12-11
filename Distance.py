fd04ec0fd19b6ddc33b18a94ed14667b883f3da7
Torzewicz, Michal <torzewicz.michal@gmail.com>
2018-12-11 01:07:26 +0100

    Code has been optimized

--- Distance.py
+++ Distance.py
@@ -4,20 +4,20 @@ import json
 class Distance:
     def __init__(self, start_point, end_point, gMaps):
         modes = ['driving', 'walking', 'bicycling', 'transit']
+        attributes = ['distance', 'duration']
         for i in modes:
-            try:
-                current_mode = gMaps.directions(start_point, end_point, mode=i)[0]['legs'][0]
-                setattr(self, i, {
-                    "distance": current_mode['distance']['text'],
-                    "duration": current_mode['duration']['text']
-                }
-                        )
-            except:
-                setattr(self, i, {
-                    "distance": "No data",
-                    "duration": "No data"
-                }
-                        )
+            for a in attributes:
+                try:
+                    current_mode = gMaps.directions(start_point, end_point, mode=i)[0]['legs'][0][a]['text']
+                except:
+                    current_mode = "No data"
+                if hasattr(self, i):
+                    getattr(self, i)[a] = current_mode
+                else:
+                    setattr(self, i, {
+                        a: current_mode
+                    }
+                            )
 
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
